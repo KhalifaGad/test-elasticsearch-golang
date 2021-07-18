576780445eff665839663718e87579a6c1608285
KhalifaGad <kkhalifa.gad@gmail.com>
2021-07-18 08:54:16 +0200

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/KhalifaGad/test-elasticsearch-golang
+
+go 1.16
+
+require github.com/elastic/go-elasticsearch/v7 v7.13.1
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/elastic/go-elasticsearch/v7"
+)
+
+func main() {
+
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+		},
+	}
+
+	es, err := elasticsearch.NewClient(cfg)
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	res, err := es.Info()
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+
+	defer res.Body.Close()
+
+	log.Println(res)
+}
